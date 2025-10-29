89d39d718bc7ffff9a324e0f6eda5950c9dcb609
Sergey Terentiev <sergey@terentiev.me>
2025-10-29 15:12:15 +0300

    button rec cmd

--- src/buttons.c
+++ src/buttons.c
@@ -1,13 +1,8 @@
 #include "buttons.h"
-
 #include <errno.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/sys/byteorder.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/logging/log.h>
 
 #define SW0_NODE	DT_ALIAS(sw0)
 
@@ -15,17 +10,61 @@
 #define BUTTON_PRESENT
 #endif
 
+
 #ifdef BUTTON_PRESENT
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/zbus/zbus.h>
+#include "gopro_client.h"
+
+
+
 static const struct gpio_dt_spec button_rec = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,{0});
 static struct gpio_callback button_cb_data;
+
+static const struct gopro_cmd_t  gopro_start_rec_msg = {
+    	.cmd_type = GP_CNTRL_HANDLE_CMD,
+		.len = 4,
+        .data = {0x03,0x01,0x01,0x01}
+};
+
+static const struct gopro_cmd_t gopro_stop_rec_msg = {
+    	.cmd_type = GP_CNTRL_HANDLE_CMD,
+        .len = 4,
+        .data = {0x03,0x01,0x01,0x00}
+};
+
+extern struct gopro_state_t gopro_state;
+
+ZBUS_CHAN_DECLARE(gopro_cmd_chan);
 
 LOG_MODULE_REGISTER(gopro_buttons, LOG_LEVEL_DBG);
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins){
+	int err;
 	// static uint8_t cmd_index = 0;
+	struct gopro_cmd_t *p_gopro_cmd;
 
 	LOG_INF("Button pressed at %" PRIu32 " pins: %d", k_cycle_get_32(),pins);
-	
+
+	if(gopro_state.record == 1){
+		p_gopro_cmd = &gopro_start_rec_msg;
+	}else{
+		p_gopro_cmd = &gopro_stop_rec_msg;
+	}
+
+	err = zbus_chan_pub(&gopro_cmd_chan, p_gopro_cmd, K_NO_WAIT);
+	if(err != 0){
+	if(err == -ENOMSG){
+		LOG_ERR("Invalid Gopro state, skip cmd");
+		return;
+	}
+	LOG_ERR("CMD chan pub failed: %d",err);
+	}
+
+
 	// if(pins & BIT(button_rec.pin)){
 	// 	LOG_INF("REC Button");
 	// 	if (led.port) {
